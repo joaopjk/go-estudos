1730783b4b9a531a592b9a89cd74e9a4f8a648b3
JoaoPjk <joaopjk48@gmail.com>
2026-09-25 19:38:18 -0300

    Pacotes

---
+++ Fundamentos/Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+// Escrever algo na tela
+func Escrever() {
+	fmt.Println("Escrevendo do pacote auxiliar!")
+}
---
+++ Fundamentos/Pacotes/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"module/auxiliar"
+)
+
+func main() {
+	fmt.Println("Escrevendo do arquivo main!")
+	auxiliar.Escrever()
+	auxiliar.Escrever2()
+}
